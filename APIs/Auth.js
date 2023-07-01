1b197cd4304062e46deb9f5091be5daa84560a48
MostafaAhmeedd <mostafaa.ahmeedd55@gmail.com>
2023-07-02 00:40:27 +0300

    Duplicate admins

--- APIs/Auth.js
+++ APIs/Auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const {User} = require('../models');
 const bcrypt = require('bcrypt');
+const { Op } = require('sequelize');
 
 const jwt = require("../services/jwt");
 
@@ -37,9 +38,29 @@ router.post("/signup", async (req, res) => {
     }
   });
 
+
+
   router.post("/admin/signup", async (req, res) => {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     try {
+      const existingUser = await User.findOne({
+        where: {
+          [Op.or]: [
+            { email: req.body.email },
+            { phoneNumber: req.body.phoneNumber }
+          ]
+        }
+      });
+  
+      if (existingUser) {
+        if (existingUser.email === req.body.email) {
+          return res.send("<script>alert('Email already exists');window.location.href = '/admin-signup';</script>");
+        } else {
+          return res.send("<script>alert('Phone number already exists');window.location.href = '/admin-signup';</script>");
+        }
+      }
+  
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  
       const newUser = await User.create({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -50,23 +71,19 @@ router.post("/signup", async (req, res) => {
       });
   
       if (newUser) {
-        const user = await User.findOne({ where: { email: req.body.email } });
-        if (user) {
-          const userId = user.id;
-          const token = jwt.generateToken(userId);
-          res.render('login');
-        } else {
-          res.status(404).json({ error: 'User not found' });
-        }
+        const userId = newUser.id;
+        const token = jwt.generateToken(userId);
+        res.render('login');
       } else {
         res.status(400).json({ error: 'Error in creating new user' });
       }
-      
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Error in creating user' });
     }
   });
+  
+  
   
   router.post("/login", async(req, res) => {
     try{
