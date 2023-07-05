15751421d34fe598e1985fc27219493ff25ef3e4
MostafaAhmeedd <mostafaa.ahmeedd55@gmail.com>
2023-07-05 04:43:09 +0300

    edit in getlandmark/image (test)

--- APIs/Search.js
+++ APIs/Search.js
@@ -57,52 +57,99 @@ router.get('/getlandmark', authenticateUser, async(req, res)=>{
     }
 })
 
-router.get('/getlandmark/image', upload.single('image'), async(req,res)=>{
+// router.get('/getlandmark/image', upload.single('image'), async(req,res)=>{
 
 
-    const projectDir = process.cwd();
+//     const projectDir = process.cwd();
 
-    let pyshell = new PythonShell(`${projectDir}/Model/Script.py`);
+//     let pyshell = new PythonShell(`${projectDir}/Model/Script.py`);
     
-        // sends a message to the Python script via stdin
-        pyshell.send(path.join(projectDir, req.file.path));
+//         // sends a message to the Python script via stdin
+//         pyshell.send(path.join(projectDir, req.file.path));
     
-        pyshell.on('message', async function (message) {
-        // received a message sent from the Python script (a simple "print" statement)
-        // res.json({result: message})
+//         pyshell.on('message', async function (message) {
+//         // received a message sent from the Python script (a simple "print" statement)
+//         // res.json({result: message})
     
-            try{
-                const landmark = await Landmark.findOne({
-                    where: {
-                    title: req.query.title
-                    },
-                    include: ['image', 'location']
-                });
+//             try{
+//                 const landmark = await Landmark.findOne({
+//                     where: {
+//                     title: req.query.title
+//                     },
+//                     include: ['image', 'location']
+//                 });
     
-                if (landmark){
-                    Search.create({
-                        person_id: req.user.id,
-                        landmark_id: landmark.id
-                    })
-                    res.status(200).json({landmark})
-                }else{
-                    res.status(404).json({message: 'not found'})
-                }
+//                 if (landmark){
+//                     Search.create({
+//                         person_id: req.user.id,
+//                         landmark_id: landmark.id
+//                     })
+//                     res.status(200).json({landmark})
+//                 }else{
+//                     res.status(404).json({message: 'not found'})
+//                 }
     
-            }catch{
-                res.status(500).json({message: 'error'})
+//             }catch{
+//                 res.status(500).json({message: 'error'})
     
-            }
+//             }
+//         });
+    
+//         // end the input stream and allow the process to exit
+//         pyshell.end(function (err,code,signal) {
+//         if (err) throw err;
+//         console.log('The exit code was: ' + code);
+//         console.log('The exit signal was: ' + signal);
+//         console.log('finished');
+//         });
+//     })
+
+
+router.get('/getlandmark/image', upload.single('image'), async (req, res) => { //test bas mn chatgbt el main fo2 ma3molo comment
+  const projectDir = process.cwd();
+
+  let pyshell = new PythonShell(`${projectDir}/Model/Script.py`);
+
+  // Sends a message to the Python script via stdin
+  pyshell.send(path.join(projectDir, req.file.path));
+
+  pyshell.on('message', function (message) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const landmark = await Landmark.findOne({
+          where: {
+            title: req.query.title,
+          },
+          include: ['image', 'location'],
         });
-    
-        // end the input stream and allow the process to exit
-        pyshell.end(function (err,code,signal) {
-        if (err) throw err;
-        console.log('The exit code was: ' + code);
-        console.log('The exit signal was: ' + signal);
-        console.log('finished');
-        });
-    })
+
+        if (landmark) {
+          Search.create({
+            person_id: req.user.id,
+            landmark_id: landmark.id,
+          });
+          res.status(200).json({ landmark });
+        } else {
+          res.status(404).json({ message: 'Not found' });
+        }
+        resolve();
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error occurred' });
+        reject(error);
+      }
+    });
+  });
+
+  // End the input stream and allow the process to exit
+  pyshell.end(function (err, code, signal) {
+    if (err) throw err;
+    console.log('The exit code was: ' + code);
+    console.log('The exit signal was: ' + signal);
+    console.log('Finished');
+  });
+});
+
 
 router.get('/gethistory', authenticateUser, async(req,res)=>{
     
