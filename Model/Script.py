36203f7827365c9fbcead816e9f5fa225e2ee13a
MostafaAhmeedd <mostafaa.ahmeedd55@gmail.com>
2023-07-07 17:35:46 +0300

    model changed

--- Model/Script.py
+++ Model/Script.py
@@ -12,7 +12,7 @@ from keras.applications.imagenet_utils import decode_predictions, preprocess_inp
 from scipy import spatial
 import pickle
 from scipy import spatial
-from tensorflow.keras.applications.vgg16 import VGG16  , preprocess_input
+from tensorflow.keras.applications.vgg19 import VGG19  , preprocess_input
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.models import Model
 def load_dictionary_from_disk(filepath):
@@ -22,9 +22,7 @@ def load_dictionary_from_disk(filepath):
 # current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path_features = os.path.join(os.getcwd(), "Model/feature_vectors.pkl")
 Feature_vectors =load_dictionary_from_disk(file_path_features)
-model = VGG16(include_top=False)
-
-
+model = VGG19(include_top=False)
 
 def get_feature(path):
     img = load_img(path,target_size=(224,224))
